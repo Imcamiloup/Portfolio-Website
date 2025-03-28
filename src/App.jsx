a64daefe7e19bf2e4167a9276a41d67c6e8e2933
Luis Gómez <luiscgr97@gmail.com>
2025-03-28 11:47:16 -0500

    change for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio-Website">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
